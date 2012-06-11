3c7b7837f0acc0f4de70559bb6a743d77134da04
poilvert <nicolas.poilvert@gmail.com>
2012-06-10 21:58:18 -0400

    ENH: simple test against numpy for fast zmuv

---
+++ bangreadout/test_zmuv.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Authors: Nicolas Pinto <nicolas.pinto@gmail.com>
+#          Nicolas Poilvert <nicolas.poilvert@gmail.com>
+
+# Licence: BSD
+
+"""
+Simple test for Cython zmuv function
+"""
+
+import numpy as np
+from numpy.testing import assert_allclose
+
+from fast_zmuv import zmuv
+
+RTOL = 1e-5
+ATOL = 1e-6
+
+
+def test_zmuv_against_numpy():
+
+    arr = np.random.randn(10000,100).astype(np.float32)
+
+    # -- numpy reference
+    ref = (arr - arr.mean(axis=1)[..., np.newaxis]) / arr.std(axis=1)[..., np.newaxis]
+
+    # -- Cython output
+    res = zmuv(arr)
+
+    assert_allclose(ref, res, rtol=RTOL, atol=ATOL)
