5573a3741e281a88f94101b6fa9a976891e8b594
Nicolas Pinto <pinto@alum.mit.edu>
2012-06-19 10:40:36 -0400

    ENH: add zmuv_rows_inplace

--- bangreadout/tests/test_zmuv.py
+++ bangreadout/tests/test_zmuv.py
@@ -10,7 +10,7 @@
 import numpy as np
 from numpy.testing import assert_allclose
 
-from bangreadout import zmuv_rows
+from bangreadout.zmuv import zmuv_rows, zmuv_rows_inplace
 
 RTOL = 1e-5
 ATOL = 1e-6
@@ -27,3 +27,16 @@ def test_zmuv_against_numpy():
     res = zmuv_rows(arr)
 
     assert_allclose(ref, res, rtol=RTOL, atol=ATOL)
+
+
+def test_zmuv_rows_inplace():
+
+    arr = np.random.randn(10000, 100).astype(np.float32)
+
+    # -- numpy reference
+    ref = (arr - arr.mean(axis=1)[:, np.newaxis]) / arr.std(axis=1)[:, np.newaxis]
+
+    # -- Cython output
+    zmuv_rows_inplace(arr)
+
+    assert_allclose(ref, arr, rtol=RTOL, atol=ATOL)
--- bangreadout/zmuv.pyx
+++ bangreadout/zmuv.pyx
@@ -58,3 +58,44 @@ def zmuv_rows(
             out[i, j] = std_inv[i] * (arr[i, j] - mean[i])
 
     return out
+
+
+def zmuv_rows_inplace(
+    np.ndarray[DTYPE_t, ndim=2] arr
+    ):
+
+    cdef Py_ssize_t rows = arr.shape[0]
+    cdef Py_ssize_t cols = arr.shape[1]
+
+    cdef np.ndarray[DTYPE_t, ndim=1] mean = np.zeros(rows, dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t, ndim=1] std = np.zeros(rows, dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t, ndim=1] std_inv = np.zeros(rows, dtype=DTYPE)
+    #cdef np.ndarray[DTYPE_t, ndim=2] out = np.zeros((rows, cols), dtype=DTYPE)
+
+    cdef Py_ssize_t i
+    cdef Py_ssize_t j
+    cdef DTYPE_t scaler = 1. / <DTYPE_t>cols
+
+    # -- computing the vector of mean values
+    for i in prange(rows, nogil=True):
+
+        for j in range(cols):
+            mean[i] += arr[i, j]
+        mean[i] *= scaler
+
+    # -- now we compute the vector of standard deviations
+    for i in prange(rows, nogil=True):
+
+        for j in range(cols):
+            std[i] += (arr[i, j] - mean[i]) ** 2
+        std[i] *= scaler
+
+    for i in range(rows):
+
+        std_inv[i] = 1. / (<DTYPE_t>_sqrt(<float>std[i]))
+
+    # -- computing the final output array (zero mean, unit variance of the input)
+    for i in prange(rows, nogil=True):
+
+        for j in prange(cols):
+            arr[i, j] = std_inv[i] * (arr[i, j] - mean[i])
