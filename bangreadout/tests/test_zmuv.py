4268f147dde9a3a8f70582973d7d7d6008638795
Nicolas Pinto <pinto@alum.mit.edu>
2012-08-28 15:07:19 -0400

    ENH: add more capability to zmuv cython/openmp code

--- bangreadout/tests/test_zmuv.py
+++ bangreadout/tests/test_zmuv.py
@@ -10,23 +10,30 @@
 import numpy as np
 from numpy.testing import assert_allclose
 
-from bangreadout.zmuv import zmuv_rows, zmuv_rows_inplace
+from bangreadout.zmuv import (
+    zmuv_rows,
+    zmuv_rows_apply_inplace,
+    zmuv_rows_inplace
+    )
 
 RTOL = 1e-5
 ATOL = 1e-6
 
 
-def test_zmuv_against_numpy():
+def test_zmuv_rows():
 
-    arr = np.random.randn(10000,100).astype(np.float32)
+    arr = np.random.randn(10000, 100).astype(np.float32)
 
     # -- numpy reference
-    ref = (arr - arr.mean(axis=1)[:, np.newaxis]) / arr.std(axis=1)[:, np.newaxis]
+    gt = (arr - arr.mean(0)) / arr.std(0)
 
-    # -- Cython output
-    res = zmuv_rows(arr)
-
-    assert_allclose(ref, res, rtol=RTOL, atol=ATOL)
+    # -- cython
+    rows_mean, rows_std = zmuv_rows(arr.T)
+    gv = (arr - rows_mean) / rows_std
+    assert_allclose(gt, gv, rtol=RTOL, atol=ATOL)
+    # inplace
+    zmuv_rows_apply_inplace(arr.T, rows_mean, rows_std)
+    assert_allclose(gt, arr, rtol=RTOL, atol=ATOL)
 
 
 def test_zmuv_rows_inplace():
@@ -34,7 +41,8 @@ def test_zmuv_rows_inplace():
     arr = np.random.randn(10000, 100).astype(np.float32)
 
     # -- numpy reference
-    ref = (arr - arr.mean(axis=1)[:, np.newaxis]) / arr.std(axis=1)[:, np.newaxis]
+    ref = (arr - arr.mean(axis=1)[:, np.newaxis])
+    ref /= arr.std(axis=1)[:, np.newaxis]
 
     # -- Cython output
     zmuv_rows_inplace(arr)
--- bangreadout/zmuv.pyx
+++ bangreadout/zmuv.pyx
@@ -3,79 +3,81 @@
 # cython: nonecheck=False
 # cython: cdivision=True
 
+# XXX: clean this up, remove code duplication
+
 import numpy as np
-cimport numpy as np
+cimport numpy as cnp
 
 from libc.math cimport sqrt, pow
 from cython.parallel import prange
 
-DTYPE = np.float32
-ctypedef np.float32_t DTYPE_t
-
-
-# -- square root C function
+# square root C function
 cdef float _sqrt(float x):
         return sqrt(x)
 
-
 def zmuv_rows(
-    np.ndarray[DTYPE_t, ndim=2] arr
+    cnp.ndarray[cnp.float32_t, ndim=2] arr
     ):
 
     cdef Py_ssize_t rows = arr.shape[0]
     cdef Py_ssize_t cols = arr.shape[1]
 
-    cdef np.ndarray[DTYPE_t, ndim=1] mean = np.zeros(rows, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t, ndim=1] std = np.zeros(rows, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t, ndim=1] std_inv = np.zeros(rows, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t, ndim=2] out = np.zeros((rows, cols), dtype=DTYPE)
+    cdef cnp.ndarray[cnp.float32_t, ndim=1] rows_mean = np.zeros(rows, dtype=np.float32)
+    cdef cnp.ndarray[cnp.float32_t, ndim=1] rows_std = np.zeros(rows, dtype=np.float32)
 
     cdef Py_ssize_t i
     cdef Py_ssize_t j
-    cdef DTYPE_t scaler = 1. / <DTYPE_t>cols
+    cdef cnp.float32_t scaler = 1. / <cnp.float32_t>cols
 
     # -- computing the vector of mean values
     for i in prange(rows, nogil=True):
 
         for j in range(cols):
-            mean[i] += arr[i, j]
-        mean[i] *= scaler
+            rows_mean[i] += arr[i, j]
+        rows_mean[i] *= scaler
 
     # -- now we compute the vector of standard deviations
     for i in prange(rows, nogil=True):
 
         for j in range(cols):
-            std[i] += (arr[i, j] - mean[i]) ** 2
-        std[i] *= scaler
+            rows_std[i] += (arr[i, j] - rows_mean[i]) ** 2
+        rows_std[i] *= scaler
 
     for i in range(rows):
+        rows_std[i] = (<cnp.float32_t>_sqrt(<float>rows_std[i]))
 
-        std_inv[i] = 1. / (<DTYPE_t>_sqrt(<float>std[i]))
+    return rows_mean, rows_std
 
-    # -- computing the final output array (zero mean, unit variance of the input)
+
+def zmuv_rows_apply_inplace(
+    cnp.ndarray[cnp.float32_t, ndim=2] arr,
+    cnp.ndarray[cnp.float32_t, ndim=1] rows_mean,
+    cnp.ndarray[cnp.float32_t, ndim=1] rows_std,
+    ):
+
+    cdef Py_ssize_t rows = arr.shape[0]
+    cdef Py_ssize_t cols = arr.shape[1]
+
+    cdef Py_ssize_t i
     for i in prange(rows, nogil=True):
-
         for j in range(cols):
-            out[i, j] = std_inv[i] * (arr[i, j] - mean[i])
-
-    return out
+            arr[i, j] = (arr[i, j] - rows_mean[i]) / rows_std[i]
 
 
 def zmuv_rows_inplace(
-    np.ndarray[DTYPE_t, ndim=2] arr
+    cnp.ndarray[cnp.float32_t, ndim=2] arr
     ):
 
     cdef Py_ssize_t rows = arr.shape[0]
     cdef Py_ssize_t cols = arr.shape[1]
 
-    cdef np.ndarray[DTYPE_t, ndim=1] mean = np.zeros(rows, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t, ndim=1] std = np.zeros(rows, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t, ndim=1] std_inv = np.zeros(rows, dtype=DTYPE)
-    #cdef np.ndarray[DTYPE_t, ndim=2] out = np.zeros((rows, cols), dtype=DTYPE)
+    cdef cnp.ndarray[cnp.float32_t, ndim=1] mean = np.zeros(rows, dtype=np.float32)
+    cdef cnp.ndarray[cnp.float32_t, ndim=1] std = np.zeros(rows, dtype=np.float32)
+    cdef cnp.ndarray[cnp.float32_t, ndim=1] std_inv = np.zeros(rows, dtype=np.float32)
 
     cdef Py_ssize_t i
     cdef Py_ssize_t j
-    cdef DTYPE_t scaler = 1. / <DTYPE_t>cols
+    cdef cnp.float32_t scaler = 1. / <cnp.float32_t>cols
 
     # -- computing the vector of mean values
     for i in prange(rows, nogil=True):
@@ -92,8 +94,7 @@ def zmuv_rows_inplace(
         std[i] *= scaler
 
     for i in range(rows):
-
-        std_inv[i] = 1. / (<DTYPE_t>_sqrt(<float>std[i]))
+        std_inv[i] = 1. / (<cnp.float32_t>_sqrt(<float>std[i]))
 
     # -- computing the final output array (zero mean, unit variance of the input)
     for i in prange(rows, nogil=True):
@@ -103,19 +104,19 @@ def zmuv_rows_inplace(
 
 
 def zmuv_rows_inplace_untested(
-    np.ndarray[DTYPE_t, ndim=2] arr
+    cnp.ndarray[cnp.float32_t, ndim=2] arr
     ):
 
     cdef Py_ssize_t rows = arr.shape[0]
     cdef Py_ssize_t cols = arr.shape[1]
 
-    cdef np.ndarray[DTYPE_t, ndim=1] _sum = np.zeros(rows, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t, ndim=1] _ssq = np.zeros(rows, dtype=DTYPE)
+    cdef cnp.ndarray[cnp.float32_t, ndim=1] _sum = np.zeros(rows, dtype=np.float32)
+    cdef cnp.ndarray[cnp.float32_t, ndim=1] _ssq = np.zeros(rows, dtype=np.float32)
 
     cdef Py_ssize_t i
     cdef Py_ssize_t j
-    cdef DTYPE_t scaler = 1. / <DTYPE_t>cols
-    cdef DTYPE_t val, _sum_i, _ssq_i, div
+    cdef cnp.float32_t scaler = 1. / <cnp.float32_t>cols
+    cdef cnp.float32_t val, _sum_i, _ssq_i, div
 
     # -- sum and ssq
     for i in prange(rows, nogil=True):
