1780b418073b1c2e4c1eb163e5113803b220ed64
Nicolas Pinto <pinto@alum.mit.edu>
2012-10-09 23:54:03 -0700

    ENH: add alpha version of partial_fit for zscorer

--- bangreadout/tests/test_zscore.py
+++ bangreadout/tests/test_zscore.py
@@ -20,3 +20,17 @@ def test_zscorer():
     gt = (arr - arr.mean(0)) / arr.std(0)
     gv = ZScorer().fit(arr).transform(arr)
     assert_allclose(gt, gv, rtol=RTOL, atol=ATOL)
+
+
+def test_zscorer_partial_fit():
+    z = ZScorer()
+    x = np.random.randn(10, 1000).astype('f')
+    x[1::3] += 1
+    x[1::11] += 2
+    z.partial_fit(x[::3])
+    z.partial_fit(x[1::3])
+    z.partial_fit(x[2::3])
+    xm = x.mean(0)
+    xs = x.std(0)
+    assert np.linalg.norm(xm - z._rows_mean) < RTOL
+    assert np.linalg.norm(xs - z._rows_std) < RTOL
--- bangreadout/zscore.py
+++ bangreadout/zscore.py
@@ -29,11 +29,52 @@ class ZScorer(object):
     def __init__(self):
         self._rows_mean = None
         self._rows_std = None
+        self._n_samples = 0
 
     def fit(self, X):
         _check_X(X)
         self._rows_mean, self._rows_std = zmuv_rows(X.T)
         return self
+
+    def partial_fit(self, X):
+        _check_X(X)
+        rows_mean, rows_std = zmuv_rows(X.T)
+
+        if self._rows_mean is None:
+
+            self._rows_mean = rows_mean
+            self._rows_std = rows_std
+            self._n_samples = len(X)
+
+        else:
+
+            # XXX: change this, we should use sum and ssq in
+            # cython-based helpers
+
+            n_samples_new = self._n_samples + len(X)
+
+            rows_mean_new_n = (
+                self._n_samples * self._rows_mean
+                +
+                len(X) * rows_mean
+            )
+            rows_mean_new = rows_mean_new_n / n_samples_new
+
+            rows_ssq_new = (
+                self._n_samples * ((self._rows_std ** 2.)
+                                   + self._rows_mean ** 2.)
+                +
+                len(X) * ((rows_std ** 2.) + rows_mean ** 2.)
+            )
+            rows_std_new = np.sqrt(
+                rows_ssq_new / n_samples_new
+                -
+                rows_mean_new ** 2.
+                )
+
+            self._rows_mean = rows_mean_new
+            self._rows_std = rows_std_new
+            self._n_samples = n_samples_new
 
     def transform(self, X):
         _check_X(X)
